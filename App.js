ef3351ca192450de83da4bc3fa484339804783b7
Katherine Bui <kathy.codes@gmail.com>
2020-03-10 11:17:14 -0700

    Foundation

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import AppNavigator from './src/navigation';
+import { StyleSheet } from 'react-native';
+
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <AppNavigator />
+    </PaperProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/navigation/index.js
@@ -0,0 +1,28 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import ViewNotes from '../screens/ViewNotes';
+import AddNotes from '../screens/AddNotes';
+
+// https://reactnavigation.org/docs/getting-started/
+// In a web browser, you can link to different pages using an anchor (<a>) tag.
+// When the user clicks on a link, the URL is pushed to the browser history stack.
+// When the user presses the back button, the browser pops the item from the top of the history stack,
+// so the active page is now the previously visited page.
+// React Native doesn't have a built-in idea of a global history stack like a web browser does
+const StackNavigator = createStackNavigator(
+  {
+    ViewNotes: {
+      screen: ViewNotes
+    },
+    AddNotes: {
+      screen: AddNotes
+    }
+  },
+  {
+    initialRouteName: 'ViewNotes',
+    headerMode: 'none',
+    mode: 'modal',
+  }
+);
+
+export default createAppContainer(StackNavigator);
